060312e3b74d925c13a55e505ec7042b33f76024
Vinz <vincentfontaine.jobs@gmail.com>
2022-10-05 23:18:34 +0200

    split aged brie or backstage passes

--- csharpcore-Verify.xunit/GildedRose/GildedRose.cs
+++ csharpcore-Verify.xunit/GildedRose/GildedRose.cs
@@ -22,41 +22,38 @@ namespace GildedRoseKata
                 if (name.StartsWith("Sulfuras"))
                     break;
 
+                if (name == "Aged Brie")
+                {
+                    if (item.Quality < 50)
+                        item.Quality = item.Quality + 1;
 
-
-
-                if (name == "Aged Brie" || name.StartsWith("Backstage passes"))
+                    if (item.SellIn < 0 && item.Quality < 50)
+                        item.Quality = item.Quality + 1;
+                    break;
+                }
+                
+                if (name.StartsWith("Backstage passes"))
                 {
                     if (item.Quality < 50)
                     {
                         item.Quality = item.Quality + 1;
 
-                        if (name.StartsWith("Backstage passes"))
-                        {
-                            if (item.SellIn < 10)
-                                item.Quality = item.Quality + 1;
+                        if (item.SellIn < 10)
+                            item.Quality = item.Quality + 1;
 
-                            if (item.SellIn < 5)
-                                item.Quality = item.Quality + 1;
-                        }
+                        if (item.SellIn < 5)
+                            item.Quality = item.Quality + 1;
                     }
                     if (item.SellIn < 0)
-                    {
-                        if (name.StartsWith("Backstage passes"))
-                            item.Quality = 0;
-                        else
-                        {
-                            if (item.Quality < 50)
-                                item.Quality = item.Quality + 1;
-                        }
-                    }
+                        item.Quality = 0;
                     break;
                 }
 
                 if (item.Quality > 0)
                     item.Quality = item.Quality - 1;
+
                 if (item.SellIn < 0 && item.Quality > 0)
-                        item.Quality = item.Quality - 1;
+                    item.Quality = item.Quality - 1;
 
             }
         }
