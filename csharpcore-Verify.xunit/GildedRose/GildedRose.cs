0b0845a21f924434234888520e0f06418a1aeb66
Vinz <vincentfontaine.jobs@gmail.com>
2022-10-06 01:21:20 +0200

    enough refacto.

--- csharpcore-Verify.xunit/GildedRose/GildedRose.cs
+++ csharpcore-Verify.xunit/GildedRose/GildedRose.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace GildedRoseKata
 {
@@ -29,9 +28,6 @@ namespace GildedRoseKata
         {
             this.Items = Items;
         }
-
-
-
 
         public void UpdateQuality()
         {
