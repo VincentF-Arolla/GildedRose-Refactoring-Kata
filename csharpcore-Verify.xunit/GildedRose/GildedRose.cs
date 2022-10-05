4b4d36dc11cf35f6cdf6458f7c9644fb69fe349b
Vinz <vincentfontaine.jobs@gmail.com>
2022-10-05 22:25:42 +0200

    factorize Items[i] as item

--- csharpcore-Verify.xunit/GildedRose/GildedRose.cs
+++ csharpcore-Verify.xunit/GildedRose/GildedRose.cs
@@ -14,72 +14,73 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && !Items[i].Name.StartsWith("Backstage passes"))
+                var item = Items[i];
+                if (item.Name != "Aged Brie" && !item.Name.StartsWith("Backstage passes"))
                 {
-                    if (Items[i].Quality > 0)
+                    if (item.Quality > 0)
                     {
-                        if (!Items[i].Name.StartsWith("Sulfuras"))
+                        if (!item.Name.StartsWith("Sulfuras"))
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            item.Quality = item.Quality - 1;
                         }
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
 
-                        if (Items[i].Name.StartsWith("Backstage passes"))
+                        if (item.Name.StartsWith("Backstage passes"))
                         {
-                            if (Items[i].SellIn < 11)
+                            if (item.SellIn < 11)
                             {
-                                if (Items[i].Quality < 50)
+                                if (item.Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
 
-                            if (Items[i].SellIn < 6)
+                            if (item.SellIn < 6)
                             {
-                                if (Items[i].Quality < 50)
+                                if (item.Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
                         }
                     }
                 }
 
-                if (!Items[i].Name.StartsWith("Sulfuras"))
+                if (!item.Name.StartsWith("Sulfuras"))
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    item.SellIn = item.SellIn - 1;
                 }
 
-                if (Items[i].SellIn < 0)
+                if (item.SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (item.Name != "Aged Brie")
                     {
-                        if (!Items[i].Name.StartsWith("Backstage passes"))
+                        if (!item.Name.StartsWith("Backstage passes"))
                         {
-                            if (Items[i].Quality > 0)
+                            if (item.Quality > 0)
                             {
-                                if (!Items[i].Name.StartsWith("Sulfuras"))
+                                if (!item.Name.StartsWith("Sulfuras"))
                                 {
-                                    Items[i].Quality = Items[i].Quality - 1;
+                                    item.Quality = item.Quality - 1;
                                 }
                             }
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            item.Quality = item.Quality - item.Quality;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
+                        if (item.Quality < 50)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            item.Quality = item.Quality + 1;
                         }
                     }
                 }
